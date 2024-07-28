d28d4f06f9db56ecb996d0bb67b7490d561200a2
myd7349 <myd7349@gmail.com>
2024-07-28 23:09:18 +0800

    Add LSLVer example

---
+++ Examples/LSLVer/Program.cs
@@ -0,0 +1,16 @@
+﻿// Port of https://github.com/sccn/liblsl/blob/master/testing/lslver.c
+using SharpLSL;
+using SharpLSL.Interop;
+
+namespace LSLVer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"LSL version: {LSL.lsl_library_version()}");
+            Console.WriteLine(Lsl.GetLibraryInfo());
+            Console.WriteLine(Lsl.GetLocalClock());
+        }
+    }
+}
