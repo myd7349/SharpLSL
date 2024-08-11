183640ace60359f88677ce41aa2e470da9696f60
myd7349 <myd7349@gmail.com>
2024-08-11 23:06:18 +0800

    Add Kernel32.cs, LSL.Interop.cs

---
+++ Source/SharpLSL/Interop/Kernel32.cs
@@ -0,0 +1,12 @@
+﻿#if NET35
+using System.Runtime.InteropServices;
+
+namespace SharpLSL.Interop
+{
+    internal static class Kernel32
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetDllDirectory(string path);
+    }
+}
+#endif
---
+++ Source/SharpLSL/Interop/LSL.Interop.cs
@@ -0,0 +1,33 @@
+﻿#if NET35
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace SharpLSL.Interop
+{
+    public static partial class LSL
+    {
+        static LSL()
+        {
+            var searchPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //if (Environment.Is64BitProcess)
+            if (IntPtr.Size == 8)
+                searchPath = Path.Combine(searchPath, "x64");
+            else
+                searchPath = Path.Combine(searchPath, "x86");
+
+            if (Directory.Exists(searchPath))
+            {
+                Trace.WriteLine($"Search LSL dynamic library in {searchPath}.");
+                Kernel32.SetDllDirectory(searchPath);
+            }
+        }
+    }
+}
+#endif
+
+
+// References:
+// [C# preprocessor directives](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives)
