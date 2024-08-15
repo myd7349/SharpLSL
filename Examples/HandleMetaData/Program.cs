8473db3150dc47550852ec51e346ab110c2301e1
myd7349 <myd7349@gmail.com>
2024-08-15 22:09:21 +0800

    Add HandleMetaData

---
+++ Examples/HandleMetaData/Program.cs
@@ -0,0 +1,78 @@
+﻿// Port of: https://github.com/sccn/liblsl/blob/master/examples/HandleMetaData.cpp
+namespace SharpLSL.Examples
+{
+    internal class HandleMetaData
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create a new stream info and declare some meta-data (in accordance with XDF format).
+                var name = args.Length >= 1 ? args[0] : "MetaTester";
+
+                var streamInfo = new StreamInfo(name, "EEG", 8, 100, ChannelFormat.Float, "myuid9527");
+
+                var channelInfos = streamInfo.Description.AppendChild("channels");
+                var channelNames = new string[] { "C3", "C4", "Cz", "FPz", "POz", "CPz", "O1", "O2" };
+
+                foreach (var channel in channelNames)
+                {
+                    channelInfos.AppendChild("channel")
+                        .AppendChild("label", channel)
+                        .AppendChild("unit", "microvolts")
+                        .AppendChild("type", "EEG");
+                }
+
+                streamInfo.Description.AppendChild("manufacturer", "SCCN");
+                streamInfo.Description.AppendChild("cap")
+                    .AppendChild("name", "EasyCap")
+                    .AppendChild("size", "54")
+                    .AppendChild("labelscheme", "10-20");
+
+                // Create outlet for the stream.
+                using (var streamOutlet = new StreamOutlet(streamInfo))
+                {
+                    // The following could run on another computer.
+
+                    // Resolve the stream and open an inlet.
+                    var streamInfos = LSL.Resolve("name", name);
+
+                    using (var streamInlet = new StreamInlet(streamInfos[0]))
+                    {
+                        // Get the full stream info (including custom meta-data) and dissect it.
+                        using (var streamInletInfo = streamInlet.GetStreamInfo())
+                        {
+                            Console.WriteLine("The stream's XML meta-data is:");
+                            Console.WriteLine(streamInletInfo.ToXML());
+
+                            Console.WriteLine("\nThe manufacturer is: {0}",
+                                streamInletInfo.Description.GetChildValue("manufacturer"));
+
+                            // TODO: inf.desc().child("cap").child_value("size")
+                            Console.WriteLine("\nThe cap circumference is: {0}",
+                                streamInletInfo.Description.FindChild("cap").GetChildValue("size"));
+                            
+                            Console.WriteLine("\nThe channel labels are as follows:\n");
+
+                            var channel = streamInletInfo
+                                .Description
+                                .FindChild("channels")
+                                .FindChild("channel");
+                            for (int c = 0; c < streamInletInfo.ChannelCount; ++c)
+                            {
+                                Console.WriteLine("  {0}", channel.GetChildValue("label"));
+                                channel = channel.FindNextSibling();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Got an exception: {ex}");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
