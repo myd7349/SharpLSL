b5a6710c2e74eb2447757a4a16da36e5cf6aa231
myd7349 <myd7349@gmail.com>
2024-08-18 11:01:59 +0800

    Add ReceiveStringMarkers, SendStringMarkers

---
+++ Examples/ReceiveStringMarkers/Program.cs
@@ -0,0 +1,50 @@
+﻿// Port of: https://github.com/sccn/liblsl/blob/master/examples/ReceiveStringMarkers.cpp
+// This example program demonstrates how a marker string stream on the network (here, of any name)
+// can be resolved, and how the marker strings (assumed to be delivered at irregular rate) can be
+// pulled from their source.
+namespace SharpLSL.Examples
+{
+    internal class ReceiveStringMarkers
+    {
+        static void Main(string[] args)
+        {
+            var streamInlets = new List<StreamInlet>();
+
+            foreach (var streamInfo in LSL.Resolve("type", "Markers"))
+            {
+                if (streamInfo.ChannelCount != 1)
+                {
+                    Console.WriteLine($"Skipping stream {streamInfo.Name} because it has more than one channel.");
+                }
+                else
+                {
+                    streamInlets.Add(new StreamInlet(streamInfo));
+                    Console.WriteLine($"Listening to {streamInfo.Name}...");
+                }
+            }
+            
+            if (streamInlets.Count == 0)
+            {
+                Console.WriteLine("No marker stream found.");
+                return;
+            }
+
+            var sample = new string[1];
+            var startTimestamp = LSL.GetLocalClock();
+
+            while (true)
+            {
+                foreach (var streamInlet in streamInlets)
+                {
+                    var timestamp = streamInlet.PullSample(sample, 0.2);
+                    if (timestamp != 0.0)
+                    {
+                        Console.WriteLine("{0}\t{1}",
+                            timestamp - streamInlet.TimeCorrection(1) - startTimestamp,
+                            sample[0]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Examples/SendStringMarkers/Program.cs
@@ -0,0 +1,51 @@
+﻿// Port of: https://github.com/sccn/liblsl/blob/master/examples/SendStringMarkers.cpp
+// This example program offers a 1-channel stream which contains strings.
+// The stream has the "Marker" content type and irregular sampling rate.
+// The name of the stream can be chosen as a startup parameter.
+namespace SharpLSL.Examples
+{
+    internal class SendStringMarkers
+    {
+        static void Main(string[] args)
+        {
+            var name = args.Length > 0 ? args[0] : "MyEventStream";
+
+            try
+            {
+                // Make a new stream info and stream outlet.
+                using (var streamInfo = new StreamInfo(name, "Markers", 1, LSL.IrregularRate, ChannelFormat.String, "id9527"))
+                using (var streamOutlet = new StreamOutlet(streamInfo))
+                {
+                    // Send random marker strings.
+                    Console.WriteLine("Now sending markers...");
+
+                    var markerTypes = new string[]
+                    {
+                    "Test", "Blah", "Marker", "XXX", "Testtest", "Test-1-2-3"
+                    };
+
+                    var rng = new Random();
+                    var marker = new string[1];
+
+                    while (true)
+                    {
+                        // Wait for 20ms.
+                        Thread.Sleep(20);
+
+                        // And choose the marker to send.
+                        marker[0] = markerTypes[rng.Next(markerTypes.Length)];
+                        Console.WriteLine($"Now sending: {marker[0]}.");
+
+                        streamOutlet.PushSample(marker);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Got an exception: {ex}");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
