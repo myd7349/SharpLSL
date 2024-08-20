c4ec828a70ca5592127a11aebc678e50a94020c0
myd7349 <myd7349@gmail.com>
2024-08-20 21:50:03 +0800

    Update GetLocalClock example

--- Examples/GetLocalClock/Program.cs
+++ Examples/GetLocalClock/Program.cs
@@ -17,11 +17,29 @@ namespace SharpLSL.Examples
                 var elapsedTime2 = stopwatch.Elapsed;
 
                 Console.WriteLine($"{timestamp:F7} {elapsedTime} <- LSL.GetLocalClock()");
-                Console.WriteLine($"{timestamp2 / (double)TimeSpan.TicksPerSecond:F7} {elapsedTime2} <- Stopwatch.GetTimestamp() / (double)TimeSpan.TicksPerSecond");
+                Console.WriteLine($"{TimestampToSeconds(timestamp2):F7} {elapsedTime2} <- TimestampToSeconds(Stopwatch.GetTimestamp())");
             }
 
-            Console.WriteLine($"Ticks per second: {TimeSpan.TicksPerSecond}.");
             Console.WriteLine($"Is high resolution? {Stopwatch.IsHighResolution}.");
+            Console.WriteLine($"Ticks per second: {TimeSpan.TicksPerSecond}.");
+            Console.WriteLine($"Frequency: {Stopwatch.Frequency}.");
+            Console.WriteLine($"Tick frequency: {(double)TimeSpan.TicksPerSecond / Stopwatch.Frequency}.");
+        }
+
+        static double TimestampToSeconds(long timestamp)
+        {
+#if false
+            var tickFrequency = (double)TimeSpan.TicksPerSecond / Stopwatch.Frequency;
+            return timestamp * tickFrequency / TimeSpan.TicksPerSecond;
+#else
+            return (double)timestamp / Stopwatch.Frequency;
+#endif
+        }
+
+        static double TimestampToMilliseconds(long timestamp)
+        {
+            var tickFrequency = (double)TimeSpan.TicksPerSecond / Stopwatch.Frequency;
+            return timestamp * tickFrequency / TimeSpan.TicksPerMillisecond;
         }
     }
 }
@@ -32,3 +50,9 @@ namespace SharpLSL.Examples
 // [Monotonic timestamps in C#](https://antonymale.co.uk/monotonic-timestamps-in-csharp.html)
 // [How do i get from Stopwatch.GetTimestamp() to a DateTime?](https://stackoverflow.com/questions/1438652/how-do-i-get-from-stopwatch-gettimestamp-to-a-datetime)
 // [Add a monotonic clock to the framework #15207](https://github.com/dotnet/runtime/issues/15207)
+// [How do you convert Stopwatch ticks to nanoseconds, milliseconds and seconds?](https://stackoverflow.com/questions/2329079/how-do-you-convert-stopwatch-ticks-to-nanoseconds-milliseconds-and-seconds)
+// [Stopwatch.GetTimestamp Method](https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch.gettimestamp?view=net-8.0)
+// https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Diagnostics/Stopwatch.cs
+// [Stopwatch.Frequency Field](https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch.frequency?view=net-8.0)
+// > Gets the frequency of the timer as the number of ticks per second.
+// https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch.frequency?view=net-8.0
