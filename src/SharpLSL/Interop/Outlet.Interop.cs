55664d822097be24f804469cd176a2516df84220
myd7349 <myd7349@gmail.com>
2026-03-01 12:34:30 +0800

    Generate overloads via T4 template

--- src/SharpLSL/Interop/Outlet.Interop.cs
+++ src/SharpLSL/Interop/Outlet.Interop.cs
@@ -9,24 +9,6 @@ namespace SharpLSL.Interop
     public static unsafe partial class LSL
     {
         [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_sample_ctp(lsl_outlet @out, sbyte[] data, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_sample_stp(lsl_outlet @out, short[] data, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_sample_itp(lsl_outlet @out, int[] data, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_sample_ltp(lsl_outlet @out, long[] data, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_sample_ftp(lsl_outlet @out, float[] data, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_sample_dtp(lsl_outlet @out, double[] data, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern int lsl_push_sample_strtp(lsl_outlet @out, IntPtr data, double timestamp, int pushthrough);
 
         [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
@@ -45,24 +27,6 @@ namespace SharpLSL.Interop
         public static extern int lsl_push_sample_buftp(lsl_outlet @out, IntPtr[] data, uint* lengths, double timestamp, int pushthrough);
 
         [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_ctp(lsl_outlet @out, sbyte[] data, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_stp(lsl_outlet @out, short[] data, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_itp(lsl_outlet @out, int[] data, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_ltp(lsl_outlet @out, long[] data, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_ftp(lsl_outlet @out, float[] data, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_dtp(lsl_outlet @out, double[] data, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern int lsl_push_chunk_strtp(lsl_outlet @out, IntPtr data, uint data_elements, double timestamp, int pushthrough);
 
         [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
@@ -73,24 +37,6 @@ namespace SharpLSL.Interop
 
         [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern int lsl_push_chunk_buftp(lsl_outlet @out, IntPtr[] data, uint* lengths, uint data_elements, double timestamp, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_ctnp(lsl_outlet @out, sbyte[] data, uint data_elements, double[] timestamps, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_stnp(lsl_outlet @out, short[] data, uint data_elements, double[] timestamps, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_itnp(lsl_outlet @out, int[] data, uint data_elements, double[] timestamps, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_ltnp(lsl_outlet @out, long[] data, uint data_elements, double[] timestamps, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_ftnp(lsl_outlet @out, float[] data, uint data_elements, double[] timestamps, int pushthrough);
-
-        [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
-        public static extern int lsl_push_chunk_dtnp(lsl_outlet @out, double[] data, uint data_elements, double[] timestamps, int pushthrough);
 
         [DllImport("lsl", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern int lsl_push_chunk_strtnp(lsl_outlet @out, IntPtr data, uint data_elements, double* timestamps, int pushthrough);
