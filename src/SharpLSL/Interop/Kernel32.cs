4a3c69bc6693e8a9b24e352f703e244272226726
myd7349 <myd7349@gmail.com>
2026-03-08 20:15:41 +0800

    Fix System.BadImageFormatException in .NET Framework

--- src/SharpLSL/Interop/Kernel32.cs
+++ src/SharpLSL/Interop/Kernel32.cs
@@ -5,7 +5,11 @@ namespace SharpLSL.Interop
 {
     internal static class Kernel32
     {
-        [DllImport("KERNEL32.dll", EntryPoint = "SetDllDirectoryW", ExactSpelling = true, SetLastError = true)]
+        //[DllImport("KERNEL32.dll", EntryPoint = "SetDllDirectoryW", ExactSpelling = true, SetLastError = true)] // not work, System.BadImageFormatException
+        //[DllImport("KERNEL32.dll", CharSet = CharSet.Unicode, SetLastError = true)] // ok
+        //[DllImport("KERNEL32.dll", CharSet = CharSet.Unicode, EntryPoint = "SetDllDirectoryW", SetLastError = true)] // ok
+        //[DllImport("KERNEL32.dll", EntryPoint = "SetDllDirectoryW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)] // ok
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern bool SetDllDirectory(string path);
     }
 }
