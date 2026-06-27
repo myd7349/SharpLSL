86be7258bc0f5f7bb15f6c8a68788d7fa0841dc6
myd7349 <myd7349@gmail.com>
2026-06-27 12:44:01 +0800

    Add SharpLSL.Benchmark

---
+++ tests/SharpLSL.Benchmark/Benchmarks.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+
+using BenchmarkDotNet.Attributes;
+using Microsoft.VSDiagnostics;
+
+namespace SharpLSL.Benchmark
+{
+    // For more information on the VS BenchmarkDotNet Diagnosers see https://learn.microsoft.com/visualstudio/profiling/profiling-with-benchmark-dotnet
+    [CPUUsageDiagnoser]
+    public class Benchmarks
+    {
+        [GlobalSetup]
+        public void Setup()
+        {
+        }
+
+        [Benchmark]
+        public double GetLocalClock()
+        {
+            return LSL.GetLocalClock();
+        }
+
+        [Benchmark]
+        public double GetLocalClockManaged()
+        {
+            return Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency;
+        }
+    }
+}
---
+++ tests/SharpLSL.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+using BenchmarkDotNet.Running;
+
+namespace SharpLSL.Benchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
